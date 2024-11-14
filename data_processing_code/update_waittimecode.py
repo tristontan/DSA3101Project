62f2377c25dec2aa671d99e8aa67f84b0927e464
kayi-lim <e0959743@u.nus.edu>
2024-11-14 13:30:12 +0800

    Update update_waittimecode.py

--- data_processing_code/update_waittimecode.py
+++ data_processing_code/update_waittimecode.py
@@ -2,32 +2,13 @@ import pandas as pd
 
 
 ride_details_df = pd.read_csv('data/raw/uss_ride_details.csv')
-file_path = 'data/raw/uss_wait_times.csv'
-df = pd.read_csv(file_path)
+df_30min_avg = pd.read_csv("data/processed/uss_30min_interval_avg_waittimes.csv")
+df_30min_avg['Date Time'] = pd.to_datetime(df_30min_avg['Date Time'])
 
-### ---------------- Calculate 30min interval average time ---------------- ###
-print(df.head())
-df.rename(columns={'Ride': 'ride', 'Date/Time': 'datetime', 'Wait Time': 'waittime' }, inplace=True)
-print(df.head())
-# Convert the 'datetime' column to a proper datetime format
-df['datetime'] = pd.to_datetime(df['datetime'])
-# Create a new column that rounds the timestamp down to the nearest 30 minutes
-df['30min_interval'] = df['datetime'].dt.floor('30T')
-# Group by the 30-minute interval and the ride name, then calculate the average wait time
-df_30min_avg = df.groupby(['30min_interval', 'ride'], as_index=False)['waittime'].mean()
-
-# Display the processed dataframe
-#print(df_30min_avg)
-
-# Optional: Save the processed data to a new CSV file
-df_30min_avg.to_csv('data/processed/uss_30min_interval_avg_waittimes.csv', index=False)
-
-### -------------- Fill in missing rides and dates ---------------------- ####
 
 # Step 1: Get the list of all unique rides from ride_details_df
 all_rides = ride_details_df['Ride'].unique()
 
-df_30min_avg.rename(columns={'ride': 'Ride', '30min_interval': 'Date Time', 'waittime': 'Waittime' }, inplace=True)
 # Step 2: Create a dictionary of rides present per time slot
 rides_per_time_slot = df_30min_avg.groupby('Date Time')['Ride'].apply(set).to_dict()
 
@@ -50,10 +31,10 @@ updated_wait_times_df['Date Time'] = pd.to_datetime(updated_wait_times_df['Date 
 # Generate all possible 30-minute timeslots based on the range of your data
 min_time = updated_wait_times_df['Date Time'].min()
 max_time = updated_wait_times_df['Date Time'].max()
-print(min_time)
-print(max_time)
+#print(min_time)
+#print(max_time)
 all_timeslots = pd.date_range(start=min_time, end=max_time, freq='30min')
-print(all_timeslots)
+#print(all_timeslots)
 
 # Get all unique rides
 all_rides = updated_wait_times_df['Ride'].unique()
